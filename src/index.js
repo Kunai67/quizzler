cd7d43da7b386969eb91f46f64d4378268d85389
Ian Agustin <inbagustin@gmail.com>
2020-03-20 11:39:00 +0800

    Created actions and planning store structure

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+fetch('https://opentdb.com/api.php?amount=10')
+.then(res => res.json())
+.then(questions => console.log(questions));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/redux/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+
+// const store = createStore(rootReducer);
+
+const store = {
+    isStarted: true,
+    selectedSource: 'https://opentdb.com/api.php?amount=10',
+    
+}
