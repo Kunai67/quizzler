8e0b1f331666084596aef97a38c4054a2efa2970
Ian Agustin <inbagustin@gmail.com>
2020-03-25 18:03:01 +0800

    Removed test from index.js

--- src/index.js
+++ src/index.js
@@ -2,41 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-// IMPORTS FOR TEST
-import { incrementCorrect, fetchQuestions, updateStatus, changeSettings, fetchCategory } from './redux/actions';
-import { store } from './redux/store';
-
-// TEST
-// console.log("Default State");
-// console.log(store.getState());
-
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(fetchCategory());
-store.dispatch(fetchQuestions());
-
-// console.log("Start Quiz");
-// store.dispatch(updateStatus(true));
-
-// console.log("Request Questions");
-// store.dispatch(fetchQuestions(11, 10, 'easy'));
-
-// console.log("Settings Changed");
-// store.dispatch(changeSettings(11, 20, "easy"));
-
-// console.log("Answered Correctly");
-// store.dispatch(incrementCorrect());
-
-// console.log("Change Category");
-// store.dispatch(fetchQuestions(1));
-
-// console.log("Change Number of Questions");
-// store.dispatch(fetchQuestions(null, 5, null));
-
-// console.log("Change Difficulty");
-// store.dispatch(fetchQuestions(null, null, "easy"));
+import * as serviceWorker from './serviceWorker'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
